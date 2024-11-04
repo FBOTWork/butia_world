03f4e43867779ab420412bfe47d773d669d0c048
Henrivaz <henriquevaz03@gmail.com>
2024-11-04 18:26:05 -0300

    repositorio adaptado

---
+++ src/butia_world/plugins/__init__.py
@@ -0,0 +1,6 @@
+from butia_world.plugins.world_plugin import WorldPlugin
+from butia_world.plugins.heartbeat_writer import HeartbeatWriterPlugin
+from butia_world.plugins.heartbeat_reader import HeartbeatReaderPlugin
+from butia_world.plugins.pose import PosePlugin
+from butia_world.plugins.viewer_reader import ViewerReaderPlugin
+from butia_world.plugins.recognition_writer import *
